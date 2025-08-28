576ea4950d55e1a5bc17df5f41be765b32f9b983
shivamvisualpath <49201846+Shivamj10@users.noreply.github.com>
2025-08-29 02:24:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,10 +5,10 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0a716d3f3b16d290c"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "c7i-flex.large"
 }
